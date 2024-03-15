b9a3ea24bc93a0157d5ccdd95ffb44ddd7f792a7
Akshay Menon <akshaymenon@Akshays-MacBook-Pro.local>
2024-03-15 15:16:09 +0000

    Exercise 1.3

--- Part 1/anecdotes/src/App.jsx
+++ Part 1/anecdotes/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 
 function App() {
-  const anecdotes = [
-    "If it hurts, do it more often.",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-    "The only way to go fast, is to go well.",
+  const anecdotesData = [
+    {id: 1, text: "If it hurts, do it more often.", votes: 0},
+    {id: 2, text: "Adding manpower to a late software project makes it later!", votes: 0},
+    {id: 3, text: "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.", votes: 0},
+    {id: 4, text: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.", votes: 0},
+    {id: 5, text: "Premature optimization is the root of all evil.", votes: 0},
+    {id: 6, text: "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.", votes: 0},
+    {id: 7, text: "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.", votes: 0},
+    {id: 8, text: "The only way to go fast, is to go well.", votes: 0},
   ];
 
   const getRandomAnecdoteIndex = () => {
@@ -19,6 +19,7 @@ function App() {
   };
 
   const randomIndex = getRandomAnecdoteIndex();
+  const [anecdotes, setAnecdotes] = useState(anecdotesData);
   const [selectedAnecdote, setSelectedAnecdote] = useState(
     anecdotes[randomIndex]
   );
@@ -28,10 +29,21 @@ function App() {
     setSelectedAnecdote(anecdotes[randomIndex]);
   };
 
+  const voteAnecdote = () => {
+    setAnecdotes(anecdotes.map(anecdote => {
+      if (anecdote.id === selectedAnecdote.id) {
+        anecdote.votes = anecdote.votes + 1;
+      }
+      return anecdote;
+    }));
+  };
+
   return (
     <>
-      {!!selectedAnecdote && <h4>{selectedAnecdote}</h4>}
+      {!!selectedAnecdote && <h4>{selectedAnecdote.text}</h4>}
+      {!!selectedAnecdote && <h4>Has {selectedAnecdote.votes} votes </h4>}
       <button onClick={setNextAnecdote}>Next Anecdote</button>
+      <button onClick={voteAnecdote}>Vote</button>
     </>
   );
 }
