84d8996a57e9d5f9448f3a65f9eba7c9dbbf8b66
Akshay Menon <akshaymenon@Akshays-MacBook-Pro.local>
2024-03-19 16:21:27 +0000

    Ex 2.4 + 2.5

--- Part 2/courseinfo/src/App.jsx
+++ Part 2/courseinfo/src/App.jsx
@@ -1,44 +1,65 @@
 import Course from "./components/Course";
 
 const App = () => {
-  let course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  let courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  course = [course].map((course) => {
+  return courses.map((course) => {
     course.totalExercises = course.parts.reduce((totalExercises, part) => {
-      return totalExercises += part.exercises;
+      return (totalExercises += part.exercises);
     }, 0);
-    return course;
+    return (
+      <Course
+        key={course.id}
+        id={course.id}
+        course={course.name}
+        parts={course.parts}
+        totalExercises={course.totalExercises}
+      />
+    );
   });
-
-  return course.map((course) => (
-    <Course
-      key={course.id}
-      id={course.id}
-      course={course.name}
-      parts={course.parts}
-      totalExercises={course.totalExercises}
-    />
-  ));
 };
 
 export default App;
